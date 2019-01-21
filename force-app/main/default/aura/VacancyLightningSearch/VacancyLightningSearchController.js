afab23daf9dbbea474280b6e60956997cb1e7f69
Alina Utkina <montgomeri15@gmail.com>
2019-01-21 17:15:14 +0200

    Upgraded aura component with controller and styles

--- force-app/main/default/aura/VacancyLightningSearch/VacancyLightningSearchController.js
+++ force-app/main/default/aura/VacancyLightningSearch/VacancyLightningSearchController.js
@@ -41,11 +41,20 @@
     },
     
     showDetails : function(component, event, helper) {
-        let whichOneCard = event.getSource().get("v.name");
-        component.set("v.whichShowButton", whichOneCard);
+        var infos = component.find("divDetailsId"), index = event.target.closest("[data-index]").dataset.index;  //Get the index value
+		infos = infos.length ? infos : [infos];  //Normalize to array
         
         let showButton = event.getSource();
-        showButton.set("v.iconName", "utility:chevronup");
+        
+        if(showButton.get("v.iconName") == "utility:chevrondown"){
+            showButton.set("v.iconName", "utility:chevronup");
+            $A.util.removeClass(infos[index], "slds-hide");
+			$A.util.addClass(infos[index], "showDivDetails");
+        } else{
+            showButton.set("v.iconName", "utility:chevrondown");
+            $A.util.removeClass(infos[index], "showDivDetails");
+			$A.util.addClass(infos[index], "slds-hide");
+        }
     },
     
     // 2nd cmp
@@ -55,17 +64,28 @@
     	selectedVacancies.push(event.getSource().get("v.name"));
     	component.set("v.selectedVacancies", selectedVacancies);
         
-        let selectButton = event.getSource();    
+        let selectButton = event.getSource();   
         selectButton.set("v.disabled", true);
-        selectButton.set("v.class", "changeMe");        
+        selectButton.set("v.class", "changeSelectButton");
     },
     
     deselectTheCard : function(component, event, helper) {
+        let jobAdvertisements = component.get("v.jobAdvertisements");
         let selectedVacancies = component.get("v.selectedVacancies");
-        let toDeletIndex =  event.getSource().get("v.name");  //it works because of indexVar="index" in iteration and name="{!index}"
-      
+        
+        let infos = component.find("buttonSelectId");
+        
+        let toDeletIndex =  event.getSource().get("v.name");  //It works because of indexVar="index" in iteration and name="{!index}"
     	selectedVacancies.splice(toDeletIndex, 1);        
     	component.set("v.selectedVacancies", selectedVacancies);
+        
+        for(let j=0; j<jobAdvertisements.length; j++){
+        	for (let i=0; i<selectedVacancies.length; i++){
+            	if((jobAdvertisements[j].Id) == (selectedVacancies[i].Id)){
+                    console.log(jobAdvertisements[j].Id); 
+                	console.log(selectedVacancies[i].Id); 
+            	}			
+			}
+        }
     },
-    
 })
