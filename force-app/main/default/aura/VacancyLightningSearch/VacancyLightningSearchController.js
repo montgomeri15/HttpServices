4e1c807191ca6277b7668b7c97705afbcc27971e
Alina Utkina <montgomeri15@gmail.com>
2019-01-24 19:30:47 +0200

    Upgraded aura component with controller and styles

--- force-app/main/default/aura/VacancyLightningSearch/VacancyLightningSearchController.js
+++ force-app/main/default/aura/VacancyLightningSearch/VacancyLightningSearchController.js
@@ -124,41 +124,86 @@
         component.set("v.popupIsOpen", false);
     },
         
-    handleFileSelected: function(component, event, helper) {  //Вешать действие на onchange и вот так получать event.getSource().get("v.files")
-		let fileInput = component.find("file").getElement();
-		let file = fileInput.files[0];
-        let MAX_FILE_SIZE = 1048576;
+    handleFileSelected: function(component, event, helper) {
+        let files = event.getParam("files");
+        let file = files[0];
+        console.log(file);
 
-        if(file.size > MAX_FILE_SIZE){
-            alert("Too big!");
-        } else if(file.type.indexOf("image/") != 0){
-            alert("Wrong extension :P");   
-        } else{
-            let fileAsAString = "";
-            let reader = new FileReader();
-            reader.onloadend = function() {
-                fileAsAString = btoa(reader.result);
-                component.set("v.file", fileAsAString); 
-        	}
-        	reader.readAsBinaryString(file); 
-                    
-            let img = component.find("imagePreview").getElement();
-            img.src = URL.createObjectURL(file);
+        let img = component.find("imagePreview").getElement();
+        img.src = URL.createObjectURL(file);
+        
+        let action = component.get("c.save");
+        
+        
+        
+        var fr = new FileReader();
+
+        fr.onload = function() {
+            var fileContents = fr.result;
+            var base64Mark = 'base64,';
+            var dataStart = fileContents.indexOf(base64Mark) + base64Mark.length;
+
+            fileContents = fileContents.substring(dataStart);
             
-            let fullPath = document.getElementById('fotofile').value;
-            if (fullPath) {
-                let startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
-                let filename = fullPath.substring(startIndex);
-                if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
-                    filename = filename.substring(1);
-                }
-                let index = filename.lastIndexOf(".");
-                let Name = filename.substring(0, index);
-                let fileExtension = filename.substring(index);
-            }
-            component.set("v.fileName", Name);
-            component.set("v.fileExtension", fileExtension);
-        }
+            action.setParams({
+                resumeId : 'a010E000004UFP3',
+                versionData : fileContents
+      		});
+			$A.enqueueAction(action);
+
+        };
+
+        fr.readAsDataURL(file);
+        
+        
+        
+                 
 	},
+    
+    /*save : function(component) {
+        var fileInput = component.find("file").getElement();
+        var file = fileInput.files[0];
+
+        if (file.size > 1048576) {
+            alert('File size cannot exceed ' + 1048576 + ' bytes.\n' + 'Selected file size: ' + file.size);
+            return;
+        }
+
+        var fr = new FileReader();
+
+        var self = this;
+        fr.onload = function() {
+            var fileContents = fr.result;
+            var base64Mark = 'base64,';
+            var dataStart = fileContents.indexOf(base64Mark) + base64Mark.length;
+
+            fileContents = fileContents.substring(dataStart);
+
+            self.upload(component, file, fileContents);
+        };
+
+        fr.readAsDataURL(file);
+    },
+
+    upload: function(component, file, fileContents) {
+        var action = component.get("c.saveTheFiles"); 
+
+        action.setParams({
+            parentId: 'a010E000004UFP3QAO', //component.get("v.parentId"),
+            fileName: file.name,
+            base64Data: fileContents,
+            contentType: file.type
+        });
+
+        action.setCallback(this, function(a) {
+            attachId = a.getReturnValue();
+            console.log(attachId);
+        });
+
+        $A.run(function() {
+            $A.enqueueAction(action); 
+        });
+
+    }*/
    
 })
